4c4e48292928baf650f7c626ac1405e382cbb6c3
Daniel Nilsson <daniel.nilsson94@outlook.com>
2017-10-14 13:57:44 +0200

    Initial commit

---
+++ src/test/java/MainIT.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class MainIT {
+
+    @Test
+    public void hello() {
+        assertEquals(true, true);
+    }
+
+}
---
+++ src/test/java/MainTest.java
@@ -0,0 +1,15 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class MainTest {
+
+    @Test
+    public void add() {
+        int a = 4;
+        int b = 3;
+
+        assertEquals(7, a + b);
+    }
+
+}
